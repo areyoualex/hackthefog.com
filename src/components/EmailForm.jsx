2527bdf76a39ad9f849035fca6a26322f942fe74
Alex Ruiz <ruiz.alex0302@gmail.com>
2018-12-08 13:38:00 -0800

    Add EmailForm component

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
+import Button from "./Button";
+import "./EmailForm.scss";
+
 class EmailForm extends Component {
+  constructor() {
+    super();
+    this.submit = this.submit.bind(this);
+  }
+  submit(e) {
+    e.preventDefault();
+  };
   render () {
     return (
-      <form>
+      <form onSubmit={this.submit} className="email-form">
         <label>
-          Email
-        </label><br />
+          EMAIL
+        </label>
         <input type="text" />
+        <Button type="submit">
+          Akhbkhak
+        </Button>
       </form>
     )
   }
